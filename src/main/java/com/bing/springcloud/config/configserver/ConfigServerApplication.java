11c2670e2bcd2c925ebdebb44cb2c3763e2d55cf
chenbing <chenbing@192.168.0.102>
2018-02-25 21:35:13 +0800

    配置中心

---
+++ src/main/java/com/bing/springcloud/config/configserver/ConfigServerApplication.java
@@ -0,0 +1,24 @@
+package com.bing.springcloud.config.configserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+	final static Logger logger = LoggerFactory.getLogger(ConfigServerApplication.class);
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(ConfigServerApplication.class)
+				.web(true).run(args);
+		logger.debug(applicationContext.getId() + "已经启动,当前host：{}",
+				applicationContext.getEnvironment().getProperty("HOSTNAME"));
+	}
+}
